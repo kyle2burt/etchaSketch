53ac40e1e9d54a02cddf60a86739842b3286621e
Kyle Burt <kyle2burt@gmail.com>
2024-01-07 21:37:08 -0800

    Add draw and clear grid functionality
        - Converted old code into createGrid function
        - Created clearGrid function
        - Added a button to the sidebar that calls clearGrid when clicked

--- javascript.js
+++ javascript.js
@@ -1,15 +1,38 @@
-const grid = document.querySelector(".grid");
+const clearButton = document.querySelector("#clear");
 
-for (i = 0; i < 16; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    for (j = 0; j < 16; j++) {
-        const row = document.createElement("div");
-        row.textContent = j;
-        row.classList.add("row");
+createGrid();
 
-        column.appendChild(row);
+clearButton.addEventListener("click", () => {
+    clearGrid();
+});
+
+
+
+function createGrid() {
+    const grid = document.querySelector(".grid");
+
+    for (i = 0; i < 16; i++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        for (j = 0; j < 16; j++) {
+            const row = document.createElement("div");
+            row.textContent = j;
+            row.classList.add("row");
+
+            row.addEventListener("mouseover", () => { // change color on hover
+                row.style.backgroundColor = "black";
+            })
+    
+            column.appendChild(row);
+        }
+    
+        grid.appendChild(column);
     }
+}
 
-    grid.appendChild(column);
+function clearGrid() {
+    const grid = document.querySelectorAll(".row");
+    grid.forEach((square) => {
+        square.style.backgroundColor = "#393E46";
+    });
 }
